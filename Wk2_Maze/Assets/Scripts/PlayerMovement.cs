59a886bb87d79b3cc2eef7fe88fc7e3f43c4557d
tiffanyliu822 <tiffanyliu@lcad.edu>
2026-09-17 16:05:41 -0700

    Added PlayerMovement, NOT FUNCTIONAL
    Save me Giselle...save me

--- Wk2_Maze/Assets/Scripts/PlayerMovement.cs
+++ Wk2_Maze/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public Transform Position;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            move += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            move += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move += Vector3.right;
+        }
+
+
+        transform.Translate(move * moveSpeed * Time.deltaTime);
     }
 }
